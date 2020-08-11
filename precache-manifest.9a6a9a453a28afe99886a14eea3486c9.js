534093f6271328e22732d60618d501495d6b2b75
Renata Cioltea <renatacioltea@gmail.com>
2020-08-12 01:56:50 +0300

    Updates

---
+++ precache-manifest.9a6a9a453a28afe99886a14eea3486c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bcccc0f6917a1b10ee4f246ca669c4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d214ea7b224cd975dac4",
+    "url": "/tic-tac-toe/static/css/main.c0da7f5d.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/tic-tac-toe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d214ea7b224cd975dac4",
+    "url": "/tic-tac-toe/static/js/main.967827f8.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
